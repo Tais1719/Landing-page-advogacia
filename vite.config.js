0ffd048b5c52eb43aec7744b4d81b92ed3469aa6
tais1719 <taissantos123098@gmail.com>
2025-08-20 15:15:07 -0300

    commit na responssividade

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Landing-page-advogacia/', // nome do seu repositório no GitHub
   plugins: [react()],
-});
+  base: '/Landing-page-advogacia/', // 👈 nome do repositório
+})
